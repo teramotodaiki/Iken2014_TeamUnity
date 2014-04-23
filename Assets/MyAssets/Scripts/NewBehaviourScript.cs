c9f68de7b3157b3626f63eaf48d2ee807d4cf1d2
teramotodaiki <i101323@gmail.com>
2014-04-23 18:13:57 +0900

    c#変更のテスト
    NewBehaviourScript.csの内容をhelloからgood morningににました

--- Assets/MyAssets/Scripts/NewBehaviourScript.cs
+++ Assets/MyAssets/Scripts/NewBehaviourScript.cs
@@ -5,7 +5,7 @@ public class NewBehaviourScript : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-        Debug.Log("hello");
+        Debug.Log("good morning");
 	}
 	
 	// Update is called once per frame
